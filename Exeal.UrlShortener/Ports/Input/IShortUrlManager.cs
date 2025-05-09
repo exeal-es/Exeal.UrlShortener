d64370ac3cd407fff498498c7ccfe72c0ac7011c
Pedro Pardal <ppardalj@gmail.com>
2025-05-09 09:59:58 +0200

    docs: add summary blocks to primary interfaces

--- Exeal.UrlShortener/Ports/Input/IShortUrlManager.cs
+++ Exeal.UrlShortener/Ports/Input/IShortUrlManager.cs
@@ -4,6 +4,18 @@ namespace Exeal.UrlShortener.Ports.Input;
 
 public interface IShortUrlManager
 {
+    /// <summary>
+    /// Creates a new short URL for the given destination URL. Optionally, a custom slug can be provided.
+    /// </summary>
+    /// <param name="destinationUrl">The destination URL to be shortened.</param>
+    /// <param name="customSlug">An optional custom slug for the short URL. If not provided, a slug will be generated.</param>
+    /// <returns>A result containing the generated slug if successful, or an error message if the operation fails.</returns>
     Task<Result<string>> CreateAsync(string destinationUrl, string? customSlug = null);
+    
+    /// <summary>
+    /// Retrieves statistics for a short URL identified by its slug.
+    /// </summary>
+    /// <param name="slug">The unique identifier for the short URL.</param>
+    /// <returns>A result containing the statistics of the short URL if the slug exists, or an error message if it does not.</returns>
     Task<Result<ShortUrlStats>> GetStatsAsync(string slug);
 }
--- Exeal.UrlShortener/Ports/Input/IShortUrlResolver.cs
+++ Exeal.UrlShortener/Ports/Input/IShortUrlResolver.cs
@@ -4,5 +4,12 @@ namespace Exeal.UrlShortener.Ports.Input;
 
 public interface IShortUrlResolver
 {
+    /// <summary>
+    /// Resolves a short URL slug to its destination URL and tracks the click event.
+    /// </summary>
+    /// <param name="slug">The unique identifier for the short URL.</param>
+    /// <param name="ipAddress">The IP address of the user accessing the short URL.</param>
+    /// <param name="userAgent">The user agent string of the client accessing the short URL.</param>
+    /// <returns>A result containing the destination URL if the slug exists, or an error message if it does not.</returns>
     Task<Result<string>> ResolveAsync(string slug, string ipAddress, string userAgent);
 }
