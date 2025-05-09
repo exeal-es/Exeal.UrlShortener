6ef4bfeb13c44fc03b3e82f982e202adb8d44ecb
Pedro Pardal <ppardalj@gmail.com>
2025-05-09 09:07:11 +0200

    refactor: convert constructor into field initializer

--- Exeal.UrlShortener.Tests/Fakes/StaticClock.cs
+++ Exeal.UrlShortener.Tests/Fakes/StaticClock.cs
@@ -4,12 +4,7 @@ namespace Exeal.UrlShortener.Tests.Fakes;
 
 public class StaticClock : IClock
 {
-    private readonly DateTime utcNow;
-
-    public StaticClock()
-    {
-        utcNow = DateTime.UtcNow;
-    }
+    private readonly DateTime utcNow = DateTime.UtcNow;
 
     public DateTime UtcNow()
     {
